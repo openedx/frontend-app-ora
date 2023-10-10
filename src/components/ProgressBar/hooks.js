b764038a7a5a32930523aeec57d4d00633160b57
Ben Warzeski <bwarzesk@gmail.com>
2023-10-10 15:54:33 +0000

    fix: update Progress Bar component

---
+++ src/components/ProgressBar/hooks.js
@@ -0,0 +1,39 @@
+import { useParams } from 'react-router-dom';
+import { useActiveView, useIsEmbedded } from 'hooks';
+import { useStepState, useEffectiveGrade } from 'data/services/lms/hooks/selectors';
+import {
+  routeSteps,
+  stepRoutes,
+  stepStates,
+} from 'data/services/lms/constants';
+
+export const useProgressStepData = ({ step, canRevisit = false }) => {
+  const { xblockId } = useParams();
+  const isEmbedded = useIsEmbedded();
+  const activeView = useActiveView();
+  const viewStep = routeSteps[activeView];
+  const stepState = useStepState({ step });
+
+  const href = `/${stepRoutes[step]}${isEmbedded ? '/embedded' : ''}/${xblockId}`;
+  const isEnabled = (
+    (stepState === stepStates.inProgress)
+    || (canRevisit && stepState === stepStates.completed)
+  );
+  const myGrade = useEffectiveGrade();
+
+  return {
+    href,
+    isEnabled,
+    isActive: viewStep === step,
+    isComplete: stepState === stepStates.completed,
+    inProgress: stepState === stepStates.inProgress,
+    isPastDue: stepState === stepStates.closed,
+    myGrade,
+    /*
+    myGrade: { earned: 8, possible: 10 },
+    isPastDue: step === 'self',
+    */
+  };
+};
+
+export default { useProgressStepData };
--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -1,135 +1,64 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { useLocation } from 'react-router-dom';
-import classNames from 'classnames';
 
 import { useIntl } from '@edx/frontend-platform/i18n';
-import { Navbar, Nav, Icon } from '@edx/paragon';
-import { Locked, CheckCircle, Error } from '@edx/paragon/icons';
+import { Navbar } from '@edx/paragon';
 
 import {
-  useAssessmentStepConfig,
-  useProgressData,
+  useAssessmentStepOrder,
   useIsPageDataLoaded,
 } from 'data/services/lms/hooks/selectors';
+import { stepNames } from 'data/services/lms/constants';
+
+import ProgressStep from './ProgressStep';
 
 import messages from './messages';
 import './index.scss';
 
-export const ProgressStep = ({
-  children,
-  href,
-  isActive,
-  isEnabled,
-  isComplete,
-  isError,
-  number,
-}) => {
-  let icon = <Icon className="nav-icon" src={Locked} />;
-  if (isComplete) {
-    icon = <Icon className="nav-icon" src={CheckCircle} />;
-  } else if (isError) {
-    icon = <Icon className="nav-icon text-danger-300" src={Error} />;
-  } else if (number) {
-    icon = <span className="nav-icon number-icon">{number}</span>;
-  }
-  return (
-    <Nav.Link
-      href={href}
-      disabled={!isEnabled}
-      className={classNames(
-        'ora-progress-nav',
-        'px-4',
-        { 'is-active': isActive },
-      )}
-    >
-      {icon}
-      {children}
-    </Nav.Link>
-  );
-};
-ProgressStep.defaultProps = {
-  href: '#',
-  isActive: false,
-  isEnabled: false,
-  isComplete: false,
-  isError: false,
-  number: null,
-};
-ProgressStep.propTypes = {
-  children: PropTypes.node.isRequired,
-  href: PropTypes.string,
-  isActive: PropTypes.bool,
-  isEnabled: PropTypes.bool,
-  isError: PropTypes.bool,
-  isComplete: PropTypes.bool,
-  number: PropTypes.number,
+export const stepLabels = {
+  [stepNames.submission]: messages.createSubmission,
+  [stepNames.peer]: messages.peerAssess,
+  [stepNames.studentTraining]: messages.studentTraining,
+  [stepNames.self]: messages.selfAssess,
+  [stepNames.myGrades]: messages.myGrade,
 };
 
-export const SubmissionStep = () => {
-  const { formatMessage } = useIntl();
-  return (
-    <ProgressStep>{formatMessage(messages.createSubmission)}</ProgressStep>
-  );
-};
-
-export const TrainingStep = () => {
-  const { formatMessage } = useIntl();
-  return (
-    <ProgressStep>{formatMessage(messages.practice)}</ProgressStep>
-  );
-};
-
-export const SelfAssessStep = () => {
-  const { formatMessage } = useIntl();
-  return (
-    <ProgressStep>{formatMessage(messages.selfAssess)}</ProgressStep>
-  );
-};
-
-export const PeerAssessStep = () => {
-  const { formatMessage } = useIntl();
-  return (
-    <ProgressStep>{formatMessage(messages.peerAssess)}</ProgressStep>
-  );
-};
-
-export const MyGradeStep = () => {
-  const { formatMessage } = useIntl();
-  return (
-    <ProgressStep>{formatMessage(messages.myGrade)}</ProgressStep>
-  );
+export const stepCanRevisit = {
+  [stepNames.submission]: true,
+  [stepNames.peer]: true,
+  [stepNames.studentTraining]: false,
+  [stepNames.self]: false,
+  [stepNames.myGrades]: false,
 };
 
 export const ProgressBar = () => {
-  const stepConfig = useAssessmentStepConfig();
-  const progress = useProgressData();
   const isLoaded = useIsPageDataLoaded();
-  const location = useLocation();
+
+  const stepOrder = useAssessmentStepOrder();
+  const { formatMessage } = useIntl();
+
   if (!isLoaded) {
     return null;
   }
-  console.log({
-    stepConfig, progress, location,
-  });
+
+  const stepEl = (step) => (
+    stepLabels[step]
+      ? (
+        <ProgressStep
+          step={step}
+          key={step}
+          label={formatMessage(stepLabels[step])}
+          canRevisit={stepCanRevisit[step]}
+        />
+      ) : null
+  );
+
   return (
     <Navbar>
       <Navbar.Collapse className="ora-progress-nav-group">
         <hr className="ora-progress-divider" />
-        <SubmissionStep />
-        {stepConfig.order.map(step => {
-          if (step === 'peer') {
-            return <PeerAssessStep key="peer" />;
-          }
-          if (step === 'training') {
-            return <TrainingStep key="training" />;
-          }
-          if (step === 'self') {
-            return <SelfAssessStep key="self" />;
-          }
-          return null;
-        })}
-        <MyGradeStep />
+        {stepEl(stepNames.submission)}
+        {stepOrder.map(stepEl)}
+        {stepEl(stepNames.myGrades)}
       </Navbar.Collapse>
     </Navbar>
   );
