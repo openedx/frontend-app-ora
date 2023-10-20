be721e4d0b1de9cda6e466c7d9e5f95dcde4dfde
Leangseu Kim <lkim@edx.org>
2023-10-20 11:14:08 -0400

    chore: make xblockview rubric collapsible

--- src/components/Rubric/index.jsx
+++ src/components/Rubric/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import { Card } from '@edx/paragon';
+import { Card, Collapsible } from '@edx/paragon';
 import { useIntl } from '@edx/frontend-platform/i18n';
 
 import { useRubricConfig } from 'data/services/lms/hooks/selectors';
@@ -14,14 +15,23 @@ import './Rubric.scss';
 /**
  * <Rubric />
  */
-export const Rubric = () => {
+export const Rubric = ({ isCollapsible }) => {
   const { criteria } = useRubricConfig();
   const { formatMessage } = useIntl();
+
+  const Layout = isCollapsible ? Collapsible : Card;
   return (
-    <Card className="rubric-card">
-      <Card.Section className="rubric-body">
-        <h3>{formatMessage(messages.rubric)}</h3>
-        <hr className="m-2.5" />
+    <Layout
+      title={<h3>{formatMessage(messages.rubric)}</h3>}
+      className='rubric-card'
+    >
+      <Card.Section className='rubric-body'>
+        {!isCollapsible && (
+          <>
+            <h3>{formatMessage(messages.rubric)}</h3>
+            <hr className='m-2.5' />
+          </>
+        )}
         {criteria.map((criterion) => (
           <CriterionContainer
             key={criterion.name}
@@ -30,11 +40,14 @@ export const Rubric = () => {
           />
         ))}
       </Card.Section>
-    </Card>
+    </Layout>
   );
 };
 
 Rubric.propTypes = {
+  isCollapsible: PropTypes.bool,
 };
+
+Rubric.defaultProps = {};
 
 export default Rubric;
