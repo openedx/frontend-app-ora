f85bd773bf01d8b32fb3256289426b77edc03b57
Ben Warzeski <bwarzesk@gmail.com>
2023-11-20 17:13:11 +0000

    fix: progressbar opens modal in xblock view

--- src/components/ProgressBar/hooks.js
+++ src/components/ProgressBar/hooks.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useIsEmbedded, useViewStep } from 'hooks/routing';
 import { useGlobalState } from 'hooks/app';
+import { useOpenModal } from 'hooks/modal';
 import {
   stepRoutes,
   stepStates,
+  stepNames,
 } from 'constants';
 
 export const useProgressStepData = ({ step, canRevisit = false }) => {
@@ -12,16 +15,19 @@ export const useProgressStepData = ({ step, canRevisit = false }) => {
   const isEmbedded = useIsEmbedded();
   const viewStep = useViewStep();
   const { effectiveGrade, stepState } = useGlobalState({ step });
+  const openModal = useOpenModal();
 
   const href = `/${stepRoutes[step]}${isEmbedded ? '/embedded' : ''}/${courseId}/${xblockId}`;
+  const onClick = () => openModal({ view: step, title: step });
   const isActive = viewStep === step;
   const isEnabled = (
     isActive
     || (stepState === stepStates.inProgress)
     || (canRevisit && stepState === stepStates.done)
   );
+
   return {
-    href,
+    ...(viewStep === stepNames.xblock ? { onClick } : { href }),
     isEnabled,
     isActive,
     isComplete: stepState === stepStates.done,
