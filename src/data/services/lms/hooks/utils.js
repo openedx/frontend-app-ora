6e80f127e196083871206585382974d26067e9c2
Ben Warzeski <bwarzesk@gmail.com>
2023-11-25 03:09:21 +0000

    chore: lms hook utils and mockData tests

--- src/data/services/lms/hooks/utils.js
+++ src/data/services/lms/hooks/utils.js
@@ -1,23 +1,13 @@
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import { camelCaseObject } from '@edx/frontend-platform';
 
-import { progressKeys } from 'constants/mockData';
-
-import fakeData from '../fakeData';
-
 export const loadData = ({ data }) => camelCaseObject(data);
 
 export const post = (...args) => getAuthenticatedHttpClient().post(...args);
 
 export const fakeResponse = (data) => Promise.resolve(camelCaseObject(data));
 
-export const oraConfigs = {
-  [progressKeys.staffAfterSubmission]: fakeData.oraConfig.assessmentStaffAfterSubmission,
-  [progressKeys.staffAfterSelf]: fakeData.oraConfig.assessmentStaffAfterSelf,
-  default: fakeData.oraConfig.assessmentStaffAfterSelf,
-};
-
-export const logPageData = (data => {
+export const logPageData = (data) => {
   console.log({ pageData: data });
   return data;
-});
+};
