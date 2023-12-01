05bd77e35da916571a3f8e47f048f29353f02212
Ben Warzeski <bwarzesk@gmail.com>
2023-12-01 04:21:45 +0000

    chore: Rubric header

--- src/components/Rubric/index.jsx
+++ src/components/Rubric/index.jsx
@@ -22,8 +22,8 @@ export const Rubric = ({ isCollapsible }) => {
   const Layout = isCollapsible ? Collapsible : Card;
   return (
     <Layout
-      title={<h3>{formatMessage(messages.rubric)}</h3>}
-      className="rubric-card"
+      title={<h3>{formatMessage(messages.header)}</h3>}
+      className="rubric-card my-3"
       defaultOpen
     >
       <Card.Section className="rubric-body">
