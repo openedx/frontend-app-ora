29257ef6df5d8dfd38557b52957e9950974532e8
Ben Warzeski <bwarzesk@gmail.com>
2023-12-01 04:21:23 +0000

    feat: prompt titles

---
+++ src/components/Prompt/messages.js
@@ -0,0 +1,27 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+import { stepNames } from 'constants';
+
+const messages = defineMessages({
+  [stepNames.submission]: {
+    defaultMessage: 'Create a response to the prompt below',
+    description: 'Submission step prompt header',
+    id: 'frontend-app-ora.Prompt.header.submission',
+  },
+  [stepNames.studentTraining]: {
+    defaultMessage: 'Practice grading a response to the prompt below',
+    description: 'Practice step prompt header',
+    id: 'frontend-app-ora.Prompt.header.studentTraining',
+  },
+  [stepNames.self]: {
+    defaultMessage: 'Grade your own response to the prompt below',
+    description: 'Self step prompt header',
+    id: 'frontend-app-ora.Prompt.header.self',
+  },
+  [stepNames.peer]: {
+    defaultMessage: "grade your peers' responses to the prompt below",
+    description: 'Peer step prompt header',
+    id: 'frontend-app-ora.Prompt.header.peer',
+  },
+});
+
+export default messages;
