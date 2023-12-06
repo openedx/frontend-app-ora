b47e2c28cbca12adc5205a9894de980e5e093552
Leangseu Kim <lkim@edx.org>
2023-12-06 13:22:02 -0500

    chore: rewrite modal container

--- src/components/ModalContainer.jsx
+++ src/components/ModalContainer.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FullscreenModal } from '@edx/paragon';
-
-import { nullMethod } from 'utils';
 import { useORAConfigData } from 'hooks/app';
 
 import ProgressBar from 'components/ProgressBar';
 
 /* The purpose of this component is to wrap views with a header/footer for situations
- * where we need to run non-embedded
+ * where we need to run non-embedded. It is a replicated style of FullScreenModal from
+ * paragon. The reason we use this instead of FullScreenModal is because FullScreenModal
+ * is very opinionated on other components that it uses on top of it. Since we are not
+ * using the modality of the component, it would be better to just replicate the style
+ * instead of using the component.
  */
 
 const ModalContainer = ({ children }) => {
   const { title } = useORAConfigData();
   return (
-    <FullscreenModal
-      isOpen
-      onClose={nullMethod}
-      hasCloseButton={false}
-      title={title}
-      modalBodyClassName="content-body bg-light-300"
-      beforeBodyNode={<ProgressBar className="px-2" />}
-    >
-      <div className="h-100">
+    <div className="h-100">
+      <div className="sticky-top bg-white">
+        <h3 className="bg-dark text-white p-3 m-0">{title}</h3>
+        <ProgressBar className="px-2 shadow-sm" />
+      </div>
+      <div className="content-body bg-light-300 p-4">
         {children}
       </div>
-    </FullscreenModal>
+    </div>
   );
 };
 ModalContainer.propTypes = {
