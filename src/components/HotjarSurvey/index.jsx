02fcf61f0f34d7d1f01d63ff53342df98c9286b0
jszewczulak <jszewczulak@2u.com>
2023-12-11 12:44:26 -0500

    chore: lint fixes

--- src/components/HotjarSurvey/index.jsx
+++ src/components/HotjarSurvey/index.jsx
@@ -1,48 +1,45 @@
 import React, { useEffect } from 'react';
-import { useGlobalState, useStepInfo, useAssessmentStepConfig, } from 'hooks/app';
-import { stepNames, stepStates, } from 'constants';
+import { useGlobalState, useStepInfo, useAssessmentStepConfig } from 'hooks/app';
+import { stepNames, stepStates } from 'constants';
 
-function showSurvey() {
+function ShowSurvey() {
   const configInfo = useAssessmentStepConfig();
   const stepInfo = useStepInfo();
-  const isSelfRequired = configInfo.settings[stepNames.self]['required']
-  const isPeerRequired = configInfo.settings[stepNames.peer]['required']
+  const isSelfRequired = configInfo.settings[stepNames.self].required;
+  const isPeerRequired = configInfo.settings[stepNames.peer].required;
+  const stepState = useGlobalState(stepNames.self).activeStepState;
 
   let isShowSurvey = false;
   /*
     Hotjar survey widget will show for the following scenarios:
-    1. ORA is configured with self AND NOT peer assessment. 
+    1. ORA is configured with self AND NOT peer assessment.
        After completing self assessment survey will render (xblock)
-    2. ORA is configured with self AND peer assessment. Survey 
+    2. ORA is configured with self AND peer assessment. Survey
        will render when learner completed their assignments
   */
   if (isSelfRequired && !isPeerRequired) {
-    const stepState = useGlobalState(stepNames.self)["activeStepState"]
     isShowSurvey = (stepState === stepStates.done);
-
   } else if (isPeerRequired && stepInfo[stepNames.peer] != null) {
     const stepConfigInfo = configInfo.settings[stepNames.peer];
     const { minNumberToGrade } = stepConfigInfo;
     const { numberOfAssessmentsCompleted } = stepInfo[stepNames.peer];
 
-    isShowSurvey = (minNumberToGrade === numberOfAssessmentsCompleted)
-
+    isShowSurvey = (minNumberToGrade === numberOfAssessmentsCompleted);
   }
   return isShowSurvey;
-};
-
+}
 
 export const HotjarSurvey = () => {
-  const isShowSurvey = showSurvey()
+  const isShowSurvey = ShowSurvey();
   useEffect(() => {
+    console.log(isShowSurvey);
     if (isShowSurvey && window.hj) {
       window.hj('event', 'lms_openassessment_survey');
     }
   });
   return (
-    <div id="openassessment_hotjar"></div>
-
-  )
-}
+    <div id="openassessment_hotjar" />
+  );
+};
 
 export default HotjarSurvey;
