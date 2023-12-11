fac434ce0391e70902d1a3412d1dbd530b255774
jszewczulak <jszewczulak@2u.com>
2023-12-11 11:52:06 -0500

    feat: updated event dispatch logic

--- src/components/HotjarSurvey/index.jsx
+++ src/components/HotjarSurvey/index.jsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { useRef } from 'react';
+import React, { useEffect } from 'react';
 import { useGlobalState, useStepInfo, useAssessmentStepConfig, } from 'hooks/app';
 import { stepNames, stepStates, } from 'constants';
 
@@ -11,44 +8,35 @@ function showSurvey() {
   const isSelfRequired = configInfo.settings[stepNames.self]['required']
   const isPeerRequired = configInfo.settings[stepNames.peer]['required']
 
-  let show = false;
+  let isShowSurvey = false;
 
   if (isSelfRequired && !isPeerRequired) {
     const stepState = useGlobalState(stepNames.self)["activeStepState"]
-    show = (stepState === stepStates.done);
+    isShowSurvey = (stepState === stepStates.done);
 
   } else if (isPeerRequired && stepInfo[stepNames.peer] != null) {
     const stepConfigInfo = configInfo.settings[stepNames.peer];
     const { minNumberToGrade } = stepConfigInfo;
     const { numberOfAssessmentsCompleted } = stepInfo[stepNames.peer];
 
-    show = (minNumberToGrade === numberOfAssessmentsCompleted)
+    isShowSurvey = (minNumberToGrade === numberOfAssessmentsCompleted)
 
   }
-  return show;
+  return isShowSurvey;
 };
 
-function TriggerHotjar() {
-  // check to see if hotjar is available, then trigger hotjar event
-  const domRef = useRef(null);
-  const hasWindow = typeof domRef.window !== 'undefined';
-  if (hasWindow && domRef.window.hj) {
-    domRef.window.hj('event', 'lms_openassessment_survey');
-  }
-  return null;
-}
 
 export const HotjarSurvey = () => {
-  console.log(showSurvey())
-  if (showSurvey()) {
-    return (
-      <div id="openassessment_hotjar">
-        <TriggerHotjar />
-      </div>
-    );
-  } else {
-    return null;
-  }
+  const isShowSurvey = showSurvey()
+  useEffect(()=>{
+    if(isShowSurvey && window.hj){
+        window.hj('event', 'lms_openassessment_survey');   
+    }
+  });
+  return(
+    <div id="openassessment_hotjar"></div>
+
+  )
 }
 
 export default HotjarSurvey;
