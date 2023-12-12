61f595dd52d5a519747ab6fe3b80606266bac77a
Leangseu Kim <lkim@edx.org>
2023-12-12 12:00:14 -0500

    chore: update xblock prompt title

--- src/components/Prompt/index.jsx
+++ src/components/Prompt/index.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { Collapsible } from '@edx/paragon';
 
+import { useActiveStepName } from 'hooks/app';
 import { useViewStep } from 'hooks/routing';
 
 import messages from './messages';
@@ -12,7 +13,9 @@ import usePromptHooks from './hooks';
 const Prompt = ({ prompt, defaultOpen }) => {
   const { open, toggleOpen } = usePromptHooks({ defaultOpen });
   const { formatMessage } = useIntl();
-  const message = messages[useViewStep()];
+  const viewStep = useViewStep();
+  const activeStepName = useActiveStepName();
+  const message = messages[viewStep] || messages[activeStepName];
   const title = message ? formatMessage(message) : '';
   return (
     <Collapsible title={(<h3>{title}</h3>)} open={open} onToggle={toggleOpen}>
