0e533e0300f3ca838221d6d939ecb0cec8a27207
Ben Warzeski <bwarzesk@gmail.com>
2024-01-02 20:50:27 +0000

    chore: routing and modal hook tests

--- src/hooks/routing.js
+++ src/hooks/routing.js
@@ -2,5 +2,5 @@ import { useLocation } from 'react-router-dom';
 import { routeSteps } from 'constants/index';
 
 export const useActiveView = () => useLocation().pathname.split('/')[1];
-export const useIsEmbedded = () => useLocation().pathname.split('/')[2] === 'embedded';
+// export const useIsEmbedded = () => useLocation().pathname.split('/')[2] === 'embedded';
 export const useViewStep = () => routeSteps[useActiveView()];
