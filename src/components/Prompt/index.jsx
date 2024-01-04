0f5be69618e8451627699ccb07b4048294577187
Ben Warzeski <bwarzeski@edx.org>
2024-01-04 12:51:46 -0500

    fix: multiple static links (#162)

--- src/components/Prompt/index.jsx
+++ src/components/Prompt/index.jsx
@@ -17,20 +17,20 @@ const Prompt = ({ prompt, defaultOpen }) => {
   const activeStepName = useActiveStepName();
   const message = messages[viewStep] || messages[activeStepName];
   const title = message ? formatMessage(message) : '';
-  const imgRegex = /img src="\/asset-v1(.*)/g;
-  const linkRegex = /a href="\/asset-v1(.*)/g;
+  const imgRegex = /img src="\/asset-v1([^"]*)?"/g;
+  const linkRegex = /a href="\/asset-v1([^"]*)?"/g;
   const { baseAssetUrl } = useORAConfigData();
   const promptWithAssets = prompt
-    .replaceAll(imgRegex, `img src="${process.env.LMS_BASE_URL}/asset-v1$1`)
-    .replaceAll(linkRegex, `a href="${process.env.LMS_BASE_URL}/asset-v1$1`);
+    .replaceAll(imgRegex, `img src="${process.env.LMS_BASE_URL}/asset-v1$1"`)
+    .replaceAll(linkRegex, `a href="${process.env.LMS_BASE_URL}/asset-v1$1"`);
 
   const staticRegex = {
-    img: /img src="\/static\/(.*)/g,
-    link: /a href="\/static\/(.*)/g,
+    img: /img src="\/static\/([^"]*)?"/g,
+    link: /a href="\/static\/([^"]*)?"/g,
   };
   const promptWithStaticAssets = promptWithAssets
-    .replaceAll(staticRegex.img, `img src="${process.env.LMS_BASE_URL}/${baseAssetUrl}$1`)
-    .replaceAll(staticRegex.link, `a href="${process.env.LMS_BASE_URL}/${baseAssetUrl}$1`);
+    .replaceAll(staticRegex.img, `img src="${process.env.LMS_BASE_URL}/${baseAssetUrl}$1"`)
+    .replaceAll(staticRegex.link, `a href="${process.env.LMS_BASE_URL}/${baseAssetUrl}$1"`);
   return (
     <Collapsible title={(<h3 className="py-3">{title}</h3>)} open={open} onToggle={toggleOpen}>
       <div dangerouslySetInnerHTML={{ __html: promptWithStaticAssets }} />
