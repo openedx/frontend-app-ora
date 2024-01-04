c6f38fe1162cd9016c80d92fdc8389acee6010dc
Ben Warzeski <bwarzesk@gmail.com>
2024-01-04 15:29:48 +0000

    fix: prompt escaping static links

--- src/components/Prompt/index.jsx
+++ src/components/Prompt/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import { Collapsible } from '@edx/paragon';
 
-import { useActiveStepName } from 'hooks/app';
+import { useActiveStepName, useORAConfigData } from 'hooks/app';
 import { useViewStep } from 'hooks/routing';
 
 import messages from './messages';
@@ -19,12 +19,21 @@ const Prompt = ({ prompt, defaultOpen }) => {
   const title = message ? formatMessage(message) : '';
   const imgRegex = /img src="\/asset-v1(.*)/g;
   const linkRegex = /a href="\/asset-v1(.*)/g;
+  const { baseAssetUrl } = useORAConfigData();
   const promptWithAssets = prompt
     .replaceAll(imgRegex, `img src="${process.env.LMS_BASE_URL}/asset-v1$1`)
     .replaceAll(linkRegex, `a href="${process.env.LMS_BASE_URL}/asset-v1$1`);
+
+  const staticRegex = {
+    img: /img src="\/static(.*)/g,
+    link: /a href="\/static(.*)/g,
+  };
+  const promptWithStaticAssets = promptWithAssets
+    .replaceAll(staticRegex.img, `img src="${process.env.LMS_BASE_URL}/${baseAssetUrl}@$1`)
+    .replaceAll(staticRegex.link, `a href="${process.env.LMS_BASE_URL}/${baseAssetUrl}@$1`);
   return (
     <Collapsible title={(<h3 className="py-3">{title}</h3>)} open={open} onToggle={toggleOpen}>
-      <div dangerouslySetInnerHTML={{ __html: promptWithAssets }} />
+      <div dangerouslySetInnerHTML={{ __html: promptWithStaticAssets }} />
     </Collapsible>
   );
 };
