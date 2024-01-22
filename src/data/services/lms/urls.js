9ee318fcd78a3f07c50a7161f8320ff877d0ffd2
Leangseu Kim <lkim@edx.org>
2024-01-22 09:30:53 -0500

    chore: linting

--- src/data/services/lms/urls.js
+++ src/data/services/lms/urls.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  useMatch, useParams,
-} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import { StrictDict } from '@edx/react-unit-test-utils';
 import { getConfig } from '@edx/frontend-platform';
