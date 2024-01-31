940e7ffaae3a29ef703675755b0e6f0a2e3e156b
Leangseu Kim <lkim@edx.org>
2024-01-31 10:18:42 -0500

    chore: add staff prompt header

--- src/components/Instructions/index.jsx
+++ src/components/Instructions/index.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import { useIntl } from '@edx/frontend-platform/i18n';
 
-import { stepStates } from 'constants/index';
-import { useStepState } from 'hooks/app';
+import { stepNames, stepStates } from 'constants/index';
+
+import { useGlobalState } from 'hooks/app';
+import { useViewStep } from 'hooks/routing';
+import { isXblockStep } from 'utils';
 
 import useInstructionsMessage from './useInstructionsMessage';
 
@@ -12,13 +15,18 @@ import messages from './messages';
 const Instructions = () => {
   const { formatMessage } = useIntl();
   const message = useInstructionsMessage();
-  const stepState = useStepState({});
-  if (stepState !== stepStates.inProgress) {
+  const viewStep = useViewStep();
+  const { activeStepName, stepState } = useGlobalState();
+  const stepName = isXblockStep(viewStep) ? activeStepName : viewStep;
+  if (stepState !== stepStates.inProgress || stepName === stepNames.staff) {
     return null;
   }
   return (
-    <div className="py-4">
-      <p className="mb-0"><strong>{formatMessage(messages.instructions)}: </strong>{message}</p>
+    <div className='py-4'>
+      <p className='mb-0'>
+        <strong>{formatMessage(messages.instructions)}: </strong>
+        {message}
+      </p>
     </div>
   );
 };
