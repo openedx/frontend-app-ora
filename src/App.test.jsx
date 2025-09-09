44c36ffb3ad993bacbb9cc9e989623398c6cc917
Tony Busa <tony.busa@wgu.edu>
2025-09-09 07:35:22 -0600

    test: added test for routes to each component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { IntlProvider } from '@edx/frontend-platform/i18n';
 import { MemoryRouter } from 'react-router-dom';
-
 import { useHandleModalCloseEvent } from 'hooks/modal';
 
 import App from './App';
@@ -22,18 +21,33 @@ jest.mock('@edx/frontend-platform/react', () => ({
   ErrorPage: ({ message }) => <div role="alert">{message || 'Error Page'}</div>,
 }));
 
+// mock this out to avoid testing the PageRoute wrapper component here
+jest.mock('components/PageRoute', () => ({ children }) => children);
+
+jest.mock('views/AssessmentView', () => () => <div>Mocked AssessmentView</div>);
+jest.mock('views/XBlockView', () => () => <div>Mocked XBlockView</div>);
+jest.mock('views/SubmissionView', () => () => <div>Mocked SubmissionView</div>);
+jest.mock('views/GradeView', () => () => <div>Mocked GradeView</div>);
+
 const handleModalClose = jest.fn();
 const addEventListener = jest.fn();
 const removeEventListener = jest.fn();
 
 describe('App component', () => {
-  const renderWithProviders = (component) => render(
-    <MemoryRouter>
+  const renderWithProviders = (component, initialEntries = '/') => render(
+    <MemoryRouter initialEntries={[initialEntries]}>
       <IntlProvider locale="en" messages={{}}>
         {component}
       </IntlProvider>
     </MemoryRouter>,
   );
+
+  const routes = {
+    xblock: '/xblock/course123/xblock456',
+    peerAssessment: '/peer_assessment/course123/xblock456/progress789',
+    submission: '/submission/course123/xblock456/submission789',
+    graded: '/graded/course123/xblock456/submission789',
+  };
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -42,6 +56,30 @@ describe('App component', () => {
     jest
       .spyOn(window, 'removeEventListener')
       .mockImplementation(removeEventListener);
+  });
+
+  it('renders app with assessment view page', () => {
+    renderWithProviders(<App />, routes.peerAssessment);
+    expect(screen.getByText('Mocked AssessmentView')).toBeInTheDocument();
+    screen.debug();
+  });
+
+  it('renders app with xblock view page', () => {
+    renderWithProviders(<App />, routes.xblock);
+    expect(screen.getByText('Mocked XBlockView')).toBeInTheDocument();
+    screen.debug();
+  });
+
+  it('renders app with SubmissionView view page', () => {
+    renderWithProviders(<App />, routes.submission);
+    expect(screen.getByText('Mocked SubmissionView')).toBeInTheDocument();
+    screen.debug();
+  });
+
+  it('renders app with GradeView view page', () => {
+    renderWithProviders(<App />, routes.graded);
+    expect(screen.getByText('Mocked GradeView')).toBeInTheDocument();
+    screen.debug();
   });
 
   it('renders app with accessible error page fallback', () => {
